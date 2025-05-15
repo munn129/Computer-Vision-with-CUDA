b984918407c9b80fbcc3ce1772a642ac87aa42e6
문기영 <ans92175@gmail.com>
2025-05-15 10:00:48 +0900

    rearrage files

---
+++ only_cpu/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <filesystem>  // need upper version than C++ 17
+#include <opencv2/opencv.hpp>
+#include <tbb/tbb.h>
+
+int essential(void);
+
+int main(void)
+{
+
+}
---
+++ opencv_with_cuda/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <filesystem>  // need upper version than C++ 17
+#include <opencv2/opencv.hpp>
+#include <opencv2/cudafilters.hpp>
+#include <tbb/tbb.h>
+
+int essential(void);
+int streamForConcurrency(void);
+
+int main(void)
+{
+
+}
+
+int essential(void)
+{
+
+}
+
+int streamForConcurrency(void)
+{
+    
+}
